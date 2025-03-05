153cbf984c8f85ce55f2647cd162592b72c41ce6
Holly Skinner <hollyskinner97@gmail.com>
2025-03-05 16:48:04 +0000

    set up the setup page

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Installation instructions to run the project:
 How to run the webpage via localhost:
 
 1.  npm run dev = to run the website locally in browser
+
+Fake user ID: YNy86Pz4AY1Mtweqx1hn
--- app/(standard)/(home)/components/Home.jsx
+++ app/(standard)/(home)/components/Home.jsx
@@ -8,7 +8,7 @@ export default function HomePage({ user }) {
       <Button
         variant="contained"
         onClick={() => {
-          router.push("/new");
+          router.push("/setup");
         }}
       >
         Create
